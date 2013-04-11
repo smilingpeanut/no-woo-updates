547acc5938879cf9d0af9194ce5f3982fd90134f
Chris Lanphear <chris@smilingpeanut.com>
2013-04-11 14:17:17 -0600

    Initial version.

---
+++ no-woo-updates.php
@@ -0,0 +1,15 @@
+<?
+/*
+Plugin Name: No Woo Updates
+Plugin URI: http://smilingpeanut.com/
+Description: Hide the annoying WooThemes update notice.
+Author: Chris Lanphear
+Version: 1.0
+Author URI: http://smilingpeanut.com/
+*/
+if ( !function_exists('woothemes_updater_notice') ) :
+	function woothemes_updater_notice() {
+		/* This space intentionally left blank. */
+	}
+endif;
+?>
